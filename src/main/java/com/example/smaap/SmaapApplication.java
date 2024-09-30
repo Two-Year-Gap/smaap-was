85cc5e20b2bc645add8e1aa4d0141743c1e391ec
Jinwoo Lee <binaryWooo@gmail.com>
2024-09-30 13:46:31 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/com/example/smaap/SmaapApplication.java
@@ -0,0 +1,13 @@
+package com.example.smaap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmaapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmaapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/smaap/SmaapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smaap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmaapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
