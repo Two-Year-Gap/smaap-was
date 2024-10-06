4c5153ca85f00dd67684c2d69ef5b070ab62ab83
Jinwoo Lee <binaryWooo@gmail.com>
2024-10-07 03:21:45 +0900

    feat: school entity 및 enum 선언

---
+++ src/main/java/com/example/smaap/application/school/entity/School.java
@@ -0,0 +1,51 @@
+package com.example.smaap.application.school.entity;
+
+import com.example.smaap.application.school.type.SchoolGenderType;
+import com.example.smaap.application.school.type.SchoolType;
+import com.example.smaap.common.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@NoArgsConstructor
+public class School extends BaseEntity {
+    @Column(name = "name", nullable = false, length = 20)
+    private String name;
+
+    @Column(name = "student_count", nullable = false)
+    private Long studentCount;
+
+    @Column(name = "employee_count", nullable = false)
+    private Long employeeCount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type", nullable = false)
+    private SchoolType type;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "gender", nullable = false)
+    private SchoolGenderType gender;
+
+    @Column(name = "lot_number_address", length = 100)
+    private String lotNumberAddress;
+
+    @Column(name = "lot_number_zip_code", length = 6)
+    private String lotNumberZipCode;
+
+    @Column(name = "road_name_address", length = 100)
+    private String readNameAddress;
+
+    @Column(name = "road_name_zip_code", length = 5)
+    private String readNameZipCode;
+
+    @Column(name = "latitude", precision = 12, scale = 10, nullable = false)
+    private BigDecimal latitude;
+
+    @Column(name = "longitude", precision = 13, scale = 10, nullable = false)
+    private BigDecimal longitude;
+}
