331d491c6ca7e47ff499c50c5bd700a017c63579
Jinwoo Lee <binaryWooo@gmail.com>
2024-10-23 00:08:15 +0900

    feat: school repository 구현 #7

---
+++ src/main/java/com/example/smaap/domain/school/repository/SchoolRepository.java
@@ -0,0 +1,7 @@
+package com.example.smaap.domain.school.repository;
+
+import com.example.smaap.domain.school.entity.School;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SchoolRepository extends JpaRepository<School, Long> {
+}
