d17d7e1102fcfa1e1b5d2f3e9eca7384f9aef1f9
Jinwoo Lee <binaryWooo@gmail.com>
2024-10-31 09:58:31 +0900

    feat: 업종 목록 조회 api 구현 #15

--- src/main/java/com/example/smaap/domain/business/entity/Business.java
+++ src/main/java/com/example/smaap/domain/business/entity/Business.java
@@ -1,21 +1,26 @@
 package com.example.smaap.domain.business.entity;
 
 import com.example.smaap.common.entity.BaseEntity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.OneToMany;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
+@Getter
 @NoArgsConstructor
 public class Business extends BaseEntity {
     @Column(name = "name", nullable = false, length = 10)
     private String name;
 
-    @OneToMany(mappedBy = "business")
+    @JsonIgnore
+    @OneToMany(mappedBy = "business", fetch = FetchType.LAZY)
     private List<Store> stores;
 
     @Builder
