6fd29bf76be2b5f2e34146ebffc8bad451cc33b6
Jinwoo Lee <binaryWooo@gmail.com>
2024-11-03 01:50:48 +0900

    rename: controller 패키지명 수정 #17

--- src/main/java/com/example/smaap/presentation/rest/BusinessController.java
+++ src/main/java/com/example/smaap/presentation/rest/BusinessController.java
@@ -1,4 +1,4 @@
-package com.example.smaap.presentation.res;
+package com.example.smaap.presentation.rest;
 
 import com.example.smaap.domain.business.service.BusinessService;
 import io.swagger.v3.oas.annotations.Operation;
--- src/main/java/com/example/smaap/presentation/rest/HealthCheckController.java
+++ src/main/java/com/example/smaap/presentation/rest/HealthCheckController.java
@@ -1,4 +1,4 @@
-package com.example.smaap.presentation.res;
+package com.example.smaap.presentation.rest;
 
 import com.example.smaap.application.health.service.HealthCheckService;
 import io.swagger.v3.oas.annotations.Operation;
