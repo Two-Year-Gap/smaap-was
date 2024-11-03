5244751c31cf1cabd80535a886f0e347455b57eb
Jinwoo Lee <binaryWooo@gmail.com>
2024-11-03 10:13:26 +0900

    feat: district repository 정의 #17

---
+++ src/main/java/com/example/smaap/domain/region/repository/DistrictRepository.java
@@ -0,0 +1,7 @@
+package com.example.smaap.domain.region.repository;
+
+import com.example.smaap.domain.region.entity.District;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DistrictRepository extends JpaRepository<District, Long> {
+}
