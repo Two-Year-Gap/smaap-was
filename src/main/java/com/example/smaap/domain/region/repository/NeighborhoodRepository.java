e0af625d6faff4e2af744156907350991b0dc957
Jinwoo Lee <binaryWooo@gmail.com>
2024-11-25 05:12:53 +0900

    feat: 매출액에 따른 인기 상권 조회 로직 구현

--- src/main/java/com/example/smaap/domain/region/repository/NeighborhoodRepository.java
+++ src/main/java/com/example/smaap/domain/region/repository/NeighborhoodRepository.java
@@ -1,22 +1,11 @@
 package com.example.smaap.domain.region.repository;
 
 import com.example.smaap.domain.region.entity.Neighborhood;
-import com.example.smaap.presentation.dto.NeighborhoodCountDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import java.util.List;
 
-public interface NeighborhoodRepository extends JpaRepository<Neighborhood, Long> {
+public interface NeighborhoodRepository extends JpaRepository<Neighborhood, Long>, QuerydslPredicateExecutor<Neighborhood> {
     List<Neighborhood> findAllByDistrictId(Long districtId);
-
-    @Query(
-            "SELECT new com.example.smaap.presentation.dto.NeighborhoodCountDTO.Response(" +
-                    "n.id, n.name, COUNT(s.id)) " +
-                    "FROM Neighborhood n " +
-                    "LEFT JOIN n.district s " + // n.district -> n.stopres로 수정 필요
-                    "GROUP BY n.id, n.name " +
-                    "ORDER BY COUNT(s.id) DESC"
-    )
-    List<NeighborhoodCountDTO.Response> findAllByStoreCountDesc();
 }
--- src/main/java/com/example/smaap/domain/region/service/NeighborhoodService.java
+++ src/main/java/com/example/smaap/domain/region/service/NeighborhoodService.java
@@ -1,8 +1,13 @@
 package com.example.smaap.domain.region.service;
 
+import com.example.smaap.domain.payment.entity.QCardPayment;
+import com.example.smaap.domain.population.entity.QPopulation;
 import com.example.smaap.domain.region.entity.Neighborhood;
+import com.example.smaap.domain.region.entity.QNeighborhood;
 import com.example.smaap.domain.region.repository.NeighborhoodRepository;
 import com.example.smaap.presentation.dto.NeighborhoodCountDTO;
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +17,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class NeighborhoodService {
     private final NeighborhoodRepository neighborhoodRepository;
+    private final JPAQueryFactory queryFactory;
 
     public List<Neighborhood> list() {
         return neighborhoodRepository.findAll();
@@ -22,16 +28,90 @@ public class NeighborhoodService {
     }
 
     public List<NeighborhoodCountDTO.Response> list(String popularType) {
-        // 점포 수
-        return neighborhoodRepository.findAllByStoreCountDesc();
-
-        // 매출액
-        // 유동 인구 수
-        // 거주 인구 수
+        switch (popularType) {
+            case "store":
+//                return findAllByStoreCount();
+                throw new IllegalArgumentException("잘못된 정렬 기준입니다.");
+            case "sales":
+                return findAllBySales();
+            case "floating":
+                return findAllByFloatingPopulation();
+            case "resident":
+                return findAllByResidentPopulation();
+            default:
+                throw new IllegalArgumentException("잘못된 정렬 기준입니다.");
+        }
     }
 
     public Neighborhood read(Long id) {
         return neighborhoodRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 동입니다."));
     }
+
+//    private List<NeighborhoodCountDTO.Response> findAllByStoreCount() {
+//        QNeighborhood neighborhood = QNeighborhood.neighborhood;
+//        QStore store = QStore.store;
+//
+//        return queryFactory
+//                .select(Projections.constructor(NeighborhoodCountDTO.Response.class,
+//                        neighborhood.id,
+//                        neighborhood.name,
+//                        store.count()))
+//                .from(neighborhood)
+//                .leftJoin(store)
+//                .on(store.neighborhood.id.eq(neighborhood.id))
+//                .groupBy(neighborhood.id, neighborhood.name)
+//                .orderBy(store.count().desc())
+//                .fetch();
+//    }
+
+    private List<NeighborhoodCountDTO.Response> findAllBySales() {
+        QNeighborhood neighborhood = QNeighborhood.neighborhood;
+        QCardPayment sales = QCardPayment.cardPayment;
+
+        return queryFactory
+                .select(Projections.constructor(NeighborhoodCountDTO.Response.class,
+                        neighborhood.id,
+                        neighborhood.name,
+                        sales.totalAmount.sum()))
+                .from(neighborhood)
+                .leftJoin(sales)
+                .on(sales.neighborhood.id.eq(neighborhood.id))
+                .groupBy(neighborhood.id, neighborhood.name)
+                .orderBy(sales.totalAmount.sum().desc())
+                .fetch();
+    }
+
+    private List<NeighborhoodCountDTO.Response> findAllByFloatingPopulation() {
+        QNeighborhood neighborhood = QNeighborhood.neighborhood;
+        QPopulation population = QPopulation.population;
+
+        return queryFactory
+                .select(Projections.constructor(NeighborhoodCountDTO.Response.class,
+                        neighborhood.id,
+                        neighborhood.name,
+                        population.floatingCount))
+                .from(neighborhood)
+                .leftJoin(population)
+                .on(population.neighborhood.id.eq(neighborhood.id))
+                .orderBy(population.floatingCount.desc())
+                .fetch();
+    }
+
+    private List<NeighborhoodCountDTO.Response> findAllByResidentPopulation() {
+        QNeighborhood neighborhood = QNeighborhood.neighborhood;
+        QPopulation population = QPopulation.population;
+
+        return queryFactory
+                .select(Projections.constructor(NeighborhoodCountDTO.Response.class,
+                        neighborhood.id,
+                        neighborhood.name,
+                        population.residentCount))
+                .from(neighborhood)
+                .leftJoin(population)
+                .on(population.neighborhood.id.eq(neighborhood.id))
+                .orderBy(population.residentCount.desc())
+                .fetch();
+    }
+
 }
