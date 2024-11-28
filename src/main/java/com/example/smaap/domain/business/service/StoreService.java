e37f4ceb8b92515ef65730bc9451548545d6db64
Jinwoo Lee <binaryWooo@gmail.com>
2024-11-29 02:29:23 +0900

    fix: store 목록 조회 api 요청 시 동 고유번호를 통해 조회하도록 수정

--- src/main/java/com/example/smaap/domain/business/service/StoreService.java
+++ src/main/java/com/example/smaap/domain/business/service/StoreService.java
@@ -15,7 +15,7 @@ import java.util.List;
 public class StoreService {
     private final StoreRepository storeRepository;
 
-    public List<Store> list(Long businessId, String address, BigDecimal latitude, BigDecimal longitude, Long range, int size) {
+    public List<Store> list(Long businessId, Long neighborhoodId, BigDecimal latitude, BigDecimal longitude, Long range, int size) {
         QStore qStore = QStore.store;
         BooleanBuilder builder = new BooleanBuilder();
 
@@ -23,9 +23,8 @@ public class StoreService {
             builder.and(qStore.business.id.eq(businessId));
         }
 
-        if (address != null && !address.isEmpty()) {
-            builder.and(qStore.lotNumberAddress.containsIgnoreCase(address)
-                    .or(qStore.readNameAddress.containsIgnoreCase(address)));
+        if (neighborhoodId != null) {
+            builder.and(qStore.neighborhood.id.eq(neighborhoodId));
         }
 
         if (latitude != null && longitude != null) {
