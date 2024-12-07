9da86a1b383720d07b377376d54639f3d82b9dea
Jinwoo Lee <binaryWooo@gmail.com>
2024-12-07 19:36:45 +0900

    refactor: 인기 상권 목록 조회 api의 description 수정 #45

--- src/main/java/com/example/smaap/presentation/rest/NeighborhoodController.java
+++ src/main/java/com/example/smaap/presentation/rest/NeighborhoodController.java
@@ -3,6 +3,7 @@ package com.example.smaap.presentation.rest;
 import com.example.smaap.domain.region.service.NeighborhoodService;
 import com.example.smaap.domain.region.type.PopularType;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -16,8 +17,8 @@ public class NeighborhoodController {
     private final NeighborhoodService neighborhoodService;
 
     @GetMapping("")
-    @Operation(summary = "인기 상권 조회", description = "조건(PopularType)에 따른 인기 상권(동)을 조회합니다.")
-    public ResponseEntity<?> list(@RequestParam PopularType type, @RequestParam(defaultValue = "5") Long count) {
+    @Operation(summary = "인기 상권 조회", description = "조건(PopularType[STORE: 상점 수, SALES: 매출액, FLOATING: 유동인구, RESIDENT: 주거인구])에 따른 인기 상권(동)을 조회합니다.")
+    public ResponseEntity<?> list(@Parameter(description = "조회할 상권 타입") @RequestParam PopularType type, @RequestParam(defaultValue = "5") Long count) {
         return ResponseEntity.ok(neighborhoodService.list(type, count));
     }
 
