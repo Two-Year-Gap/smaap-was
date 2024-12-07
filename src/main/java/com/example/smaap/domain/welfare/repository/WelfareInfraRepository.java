286ed56424df6d85ee8da302e880843dcb907e82
Jinwoo Lee <binaryWooo@gmail.com>
2024-12-07 23:42:42 +0900

    feat: 복지성 인프라 목록 조회 시 개수 제한 파라미터 추가 #49

--- src/main/java/com/example/smaap/domain/welfare/repository/WelfareInfraRepository.java
+++ src/main/java/com/example/smaap/domain/welfare/repository/WelfareInfraRepository.java
@@ -1,8 +1,16 @@
 package com.example.smaap.domain.welfare.repository;
 
 import com.example.smaap.domain.welfare.entity.WelfareInfra;
+import com.querydsl.core.types.Predicate;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
+import java.util.List;
+
 public interface WelfareInfraRepository extends JpaRepository<WelfareInfra, Long>, QuerydslPredicateExecutor<WelfareInfra> {
+    default List<WelfareInfra> findAll(Predicate predicate, int size) {
+        return findAll((Predicate) predicate, (Pageable) PageRequest.of(0, size)).getContent();
+    }
 }
